89460ea4d26fed6bc5526f98955eee411a996ef4
SANJAAY11 <727723euec171@skcet.ac.in>
2024-12-17 10:18:22 +0530

    PHOTOS

---
+++ file1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        int x = 5;
+        int y = 6;
+        int sum = x + y;
+        System.out.println(sum); 
+    }
+}
