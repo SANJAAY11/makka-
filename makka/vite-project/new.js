647fefd1c9d968321f5194f4ad8d489b0ab1aa35
SANJAAY11 <727723euec171@skcet.ac.in>
2025-01-24 10:04:18 +0530

    guessing number

---
+++ makka/vite-project/new.js
@@ -0,0 +1,33 @@
+const user{
+    name:"Kendrick",
+    age:"35",
+    is Singer:"true",
+}
+console.log(user.name);
+
+let a=25;
+let b=a;
+b=30;
+console.log(a);
+console.log(b);
+ 
+let obj={age:22}
+let objCopy=obj
+objCopy.age=25
+console.log(obj.age);
+let user={
+    name:"Kendrick",
+    age:"35",
+    is Singer:"true",
+}
+}
+
+for(let key in user){
+    console.log(Key +":"+user[Key]);
+}
+let array=[1,2,3,4,5,6,7,8]
+
+for(let value of array){
+
+}
+
